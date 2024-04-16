9a522c1e0850f1e48d4b877e6027cb6158884093
Darkpunkki <jimi.pettila@metropolia.fi>
2024-04-16 16:05:19 +0300

    add register form

--- Code/main.js
+++ Code/main.js
@@ -26,17 +26,37 @@ document.getElementById('login').addEventListener('click', () => {
 
 // event listener for login button
 document.getElementById("login").addEventListener("click", function () {
-  var form = document.getElementById("loginForm");
-  if (form.style.display === "none") {
-    form.style.display = "block";
+  var loginForm = document.getElementById("loginForm");
+  var registerForm = document.getElementById("registerForm");
+  if (loginForm.style.display === "none" || loginForm.style.display === "") {
+    loginForm.style.display = "block";
+    registerForm.style.display = "none"; // Hide the register form if login form is shown
   } else {
-    form.style.display = "none";
+    loginForm.style.display = "none";
   }
 });
 
+// event listener for register button
+document.getElementById("register").addEventListener("click", function () {
+  var registerForm = document.getElementById("registerForm");
+  var loginForm = document.getElementById("loginForm");
+  if (
+    registerForm.style.display === "none" ||
+    registerForm.style.display === ""
+  ) {
+    registerForm.style.display = "block";
+    loginForm.style.display = "none"; // Hide the login form if register form is shown
+  } else {
+    registerForm.style.display = "none";
+  }
+});
+
+// DEV only
 // event listener for submit button, directs to login page
 // to be replaced later with actual login functionality
-document.getElementById("submit-btn").addEventListener("click", function (e) {
-  e.preventDefault();
-  window.location = "login.html";
-});
+document
+  .getElementById("login-submit-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    window.location = "login.html";
+  });
