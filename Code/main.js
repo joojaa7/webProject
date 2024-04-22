5b1ffe92ee989a1530f00bd0bba83a2627268404
joojaa7 <Mikael.Asikainen@gmail.com>
2024-04-22 14:22:47 +0300

    Data demO

--- Code/main.js
+++ Code/main.js
@@ -1,5 +1,18 @@
 console.log("In use.");
 
+const date = "01.01.2024";
+
+const data = [
+  {
+    Nimi: "Juustohamppari",
+    Pvm: "01.01.2024",
+  },
+  {
+    Nimi: "Epäjuustohamppari",
+    Pvm: "02.01.2024",
+  },
+];
+
 const weekdayButtons = document.getElementsByClassName("weekday_link");
 
 for (let button of weekdayButtons) {
@@ -7,14 +20,14 @@ for (let button of weekdayButtons) {
     console.log(e);
     console.log(e.target.innerText);
     document.getElementsByClassName("menu_items")[0].innerHTML = `
-        <p>Menu for: ${e.target.innerText}</p>
-        <div class="menu_entry">
-            <img src="hampurilaiset.jpg" alt="hampurilaiset" class="menu_item_image">
-            <div class="item_description">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos perspiciatis consequatur ab aspernatur explicabo dolore minima, 
-                    ipsam nostrum nihil fugit, voluptates distinctio aperiam numquam rerum tempore maiores rem soluta ducimus?</p>
-            </div>
-        </div>`;
+            <p>Menu for: ${e.target.innerText}</p>
+            <div class="menu_entry">
+                <img src="hampurilaiset.jpg" alt="hampurilaiset" class="menu_item_image">
+                <div class="item_description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos perspiciatis consequatur ab aspernatur explicabo dolore minima, 
+                        ipsam nostrum nihil fugit, voluptates distinctio aperiam numquam rerum tempore maiores rem soluta ducimus?</p>
+                </div>
+            </div>`;
     // Elementit luodaat dynaamisesti datasta.s
   });
 }
